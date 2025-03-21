d08017329a8291dbddefd6f293615537a4da6958
muhammed sinan <muhammedsinan7749@gmail.com>
2025-03-21 10:44:43 +0530

    Favicon added

--- app/metadata.ts
+++ app/metadata.ts
@@ -3,4 +3,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
     title: "Hire and Retain",
     description: "ATS for Small/Micro Biz: 30-Day Free Trial, Save Big!",
+    icons: {
+        icon: '/favicon.png',
+    },
 };
