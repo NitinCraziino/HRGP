d4f25029140efe19c2ae7c194503926be7037aa9
muhammed sinan <muhammedsinan7749@gmail.com>
2025-04-07 18:54:42 +0530

    Loacation managing added

---
+++ client/components/page-components/company-profile/LocationFormInputs.tsx
@@ -0,0 +1,167 @@
+import { FieldErrors, UseFormRegister, UseFormSetValue, UseFormSetError, useWatch, Control, UseFormGetValues, UseFormWatch } from "react-hook-form";
+import { InputWithError } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { getLocationByPostalCode } from "@/lib/api/publicRoutes";
+import useIsLoading from "@/hooks/states/useIsLoading";
+import { LocationFormInputs } from "./LocationsSection";
+import ButtonWithLoading from "@/components/ButtonWithLoading";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+type LocationFormInputsProps = {
+    register: UseFormRegister<LocationFormInputs>;
+    errors: FieldErrors<LocationFormInputs>;
+    setValue: UseFormSetValue<LocationFormInputs>;
+    setError: UseFormSetError<LocationFormInputs>;
+    control: Control<LocationFormInputs>;
+    values?: {
+        addressType: string;
+        address: string;
+        city: string;
+        state: string;
+        country: string;
+        postalCode: string;
+    };
+    onSubmit: (data: LocationFormInputs) => void;
+    getValues: UseFormGetValues<LocationFormInputs>;
+    isLoading: boolean;
+    watch: UseFormWatch<LocationFormInputs>;
+};
+
+const LocationFormInput = ({
+    register,
+    errors,
+    setValue,
+    setError,
+    control,
+    values,
+    onSubmit,
+    getValues,
+    isLoading,
+    watch
+}: LocationFormInputsProps) => {
+    const [isDisabled, setIsDisabled] = useState(false);
+    const setIsLoading = useIsLoading((state) => state.setIsLoading);
+
+    const postalCode = useWatch({ control, name: "postalCode" });
+
+    useEffect(() => {
+        if (values) {
+            setValue("address", values.address);
+            setValue("country", values.country);
+            setValue("state", values.state);
+            setValue("city", values.city);
+            setValue("postalCode", values.postalCode);
+        }
+    }, [values]);
+
+    useEffect(() => {
+        if (!postalCode || postalCode.length < 4) return;
+
+        const timeout = setTimeout(async () => {
+            try {
+                setIsLoading(true);
+                const location = await getLocationByPostalCode(postalCode);
+                setValue("address", location.formattedAddress);
+                setValue("country", location.country);
+                setValue("state", location.state);
+                setValue("city", location.city);
+                setIsDisabled(true);
+            } catch (error: any) {
+                setError("postalCode", {
+                    message: error?.response?.data?.message || "Invalid postal code",
+                });
+                setValue("address", values?.address || "");
+                setValue("country", values?.country || "");
+                setValue("state", values?.state || "");
+                setValue("city", values?.city || "");
+                setIsDisabled(false);
+            } finally {
+                setIsLoading(false);
+            }
+        }, 600); // 600ms debounce
+
+        return () => clearTimeout(timeout);
+    }, [postalCode]);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        onSubmit({
+            address: getValues("address"),
+            addressType: getValues("addressType"),
+            city: getValues("city"),
+            state: getValues("state"),
+            country: getValues("country"),
+            postalCode: getValues("postalCode"),
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="mb-2">
+                <Label>Address Type</Label>
+                <Select
+                    value={watch("addressType")}
+                    onValueChange={(value) => setValue("addressType", value)}
+                >
+                    <SelectTrigger className="w-full min-h-[50px]">
+                        <SelectValue placeholder="Select Address Type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="primary">Primary</SelectItem>
+                        <SelectItem value="secondary">Secondary</SelectItem>
+                    </SelectContent>
+                </Select>
+                {errors.addressType && <p className="text-red-500 text-sm">{errors.addressType.message}</p>}
+            </div>
+            <div className="grid grid-cols-2 gap-2 space-y-2">
+                <InputWithError
+                    label="Postal Code"
+                    placeholder="Postal Code"
+                    className="py-0 px-4 w-full min-h-[50px]"
+                    {...register("postalCode")}
+                    error={errors.postalCode?.message}
+                />
+                <InputWithError
+                    label="Street Address"
+                    placeholder="Street Address"
+                    className="py-0 px-4 w-full min-h-[50px]"
+                    {...register("address")}
+                    error={errors.address?.message}
+                />
+                <InputWithError
+                    label="City"
+                    placeholder="City"
+                    className="py-0 px-4 w-full min-h-[50px]"
+                    {...register("city")}
+                    error={errors.city?.message}
+                />
+                <InputWithError
+                    label="State"
+                    placeholder="State"
+                    disabled={isDisabled}
+                    className="py-0 px-4 w-full min-h-[50px]"
+                    {...register("state")}
+                    error={errors.state?.message}
+                />
+                <InputWithError
+                    label="Country"
+                    placeholder="Country"
+                    className="py-0 px-4 w-full min-h-[50px]"
+                    {...register("country")}
+                    disabled={isDisabled}
+                    error={errors.country?.message}
+                />
+            </div>
+            <ButtonWithLoading
+                isLoading={isLoading}
+                type="submit"
+                className="mt-4 py-2 px-8 rounded bg-[#5d45f8] hover:bg-[#4a35d9] text-sm float-right min-w-[100px]"
+            >
+                Save
+            </ButtonWithLoading>
+        </form>
+    );
+};
+
+
+export default LocationFormInput;
--- client/components/page-components/company-profile/LocationsSection.tsx
+++ client/components/page-components/company-profile/LocationsSection.tsx
@@ -1,72 +1,168 @@
-import { Pencil, Trash } from 'lucide-react';
+'use client';
+import { Pencil, Trash, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
-import React from 'react';
+import { Table, TableHead, TableBody, TableRow, TableCell, TableHeader } from '@/components/ui/table'; // Import shadcn table components
+import { useState } from 'react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import LocationFormInputs from './LocationFormInputs';
+
+const locations = [
+    {
+        addressId: 1,
+        addressType: 'Primary',
+        address: '45 Meetinghouse Lane #1083',
+        city: 'Sagamore Beach',
+        state: 'MA',
+        country: 'United States',
+        postalCode: '02562',
+    },
+    {
+        addressId: 2,
+        addressType: 'Branch',
+        address: 'Calgiri',
+        city: 'Jaipur',
+        state: 'RJ',
+        country: 'India',
+        postalCode: '302017',
+    },
+];
+
+const LocationSchema = z.object({
+    addressType: z.string().min(1, { message: "Address type is required" }),
+    address: z.string().min(1, { message: "Address is required" }),
+    city: z.string().min(1, { message: "City is required" }),
+    state: z.string().min(1, { message: "State is required" }),
+    country: z.string().min(2, { message: "Country is required" }),
+    postalCode: z.string().min(1, { message: "Postal code is required" }),
+});
+
+export type LocationFormInputs = z.infer<typeof LocationSchema>;
 
 const LocationsSection = () => {
+    const [editMode, setEditMode] = useState<{
+        isEditing: boolean;
+        editLocation: LocationFormInputs | null;
+    }>({
+        isEditing: false,
+        editLocation: null,
+    });
+
+    const {
+        control,
+        setValue,
+        setError,
+        register,
+        formState: { errors, isSubmitting },
+        getValues,
+        clearErrors,
+        reset,
+        trigger,
+        watch
+    } = useForm<LocationFormInputs>();
+
+    const handleEditMode = () => {
+        if (editMode.isEditing) {
+            setEditMode({ ...editMode, isEditing: false });
+            clearErrors();
+            reset();
+        } else {
+            setEditMode({ ...editMode, isEditing: true });
+        }
+    };
+
+    const handleEditLocation = async (data: LocationFormInputs) => {
+        const isValid = await trigger();
+        if (!isValid) return;
+        if (editMode.editLocation) {
+            console.log(data);
+        } else {
+            console.log(data);
+        }
+    };
+
+
     return (
         <div className="mt-6">
             <div className="flex justify-between items-center p-6">
                 <h2 className="text-2xl font-bold">Location(s)</h2>
-                <Button variant="ghost" size="icon">
-                    <Plus className="h-5 w-5" />
+                <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={handleEditMode}
+                    className="bg-indigo-100 text-indigo-600 hover:bg-indigo-200 hover:text-indigo-800 rounded-full"
+                >
+                    {editMode.isEditing ? <i className='fas fa-times' /> : <i className='fas fa-plus' />}
                 </Button>
             </div>
 
-            {/* Locations Table */}
-            <div className="overflow-x-auto">
-                <table className="w-full">
-                    <thead className="bg-gray-50">
-                        <tr>
-                            <th className="text-left p-4 text-sm font-semibold">Address Type</th>
-                            <th className="text-left p-4 text-sm font-semibold">Street Address</th>
-                            <th className="text-left p-4 text-sm font-semibold">City</th>
-                            <th className="text-left p-4 text-sm font-semibold">State</th>
-                            <th className="text-left p-4 text-sm font-semibold">Country</th>
-                            <th className="text-left p-4 text-sm font-semibold">Postal Code</th>
-                            <th className="text-left p-4 text-sm font-semibold">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="border-t">
-                            <td className="p-4">Primary</td>
-                            <td className="p-4">45 Meetinghouse Lane #1083</td>
-                            <td className="p-4">Sagamore Beach</td>
-                            <td className="p-4">MA</td>
-                            <td className="p-4">United States</td>
-                            <td className="p-4">02562</td>
-                            <td className="p-4">
-                                <div className="flex space-x-2">
-                                    <Button variant="ghost" size="icon" className="text-indigo-600 hover:text-indigo-800 bg-indigo-100 rounded-full h-8 w-8">
-                                        <Pencil className="h-4 w-4" />
-                                    </Button>
-                                    <Button variant="ghost" size="icon" className="text-indigo-600 hover:text-indigo-800 bg-indigo-100 rounded-full h-8 w-8">
-                                        <Trash className="h-4 w-4" />
-                                    </Button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr className="border-t">
-                            <td className="p-4">Branch</td>
-                            <td className="p-4">Calgiri</td>
-                            <td className="p-4">Jaipur</td>
-                            <td className="p-4">RJ</td>
-                            <td className="p-4">India</td>
-                            <td className="p-4">302017</td>
-                            <td className="p-4">
-                                <div className="flex space-x-2">
-                                    <Button variant="ghost" size="icon" className="text-indigo-600 hover:text-indigo-800 bg-indigo-100 rounded-full h-8 w-8">
-                                        <Pencil className="h-4 w-4" />
-                                    </Button>
-                                    <Button variant="ghost" size="icon" className="text-indigo-600 hover:text-indigo-800 bg-indigo-100 rounded-full h-8 w-8">
-                                        <Trash className="h-4 w-4" />
-                                    </Button>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            {!editMode.isEditing && (
+                <div className="overflow-x-auto">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="text-left p-4 text-sm font-semibold">Address Type</TableHead>
+                                <TableHead className="text-left p-4 text-sm font-semibold">Street Address</TableHead>
+                                <TableHead className="text-left p-4 text-sm font-semibold">City</TableHead>
+                                <TableHead className="text-left p-4 text-sm font-semibold">State</TableHead>
+                                <TableHead className="text-left p-4 text-sm font-semibold">Country</TableHead>
+                                <TableHead className="text-left p-4 text-sm font-semibold">Postal Code</TableHead>
+                                <TableHead className="text-left p-4 text-sm font-semibold">Action</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {locations.map((location) => (
+                                <TableRow className="border-t" key={location.addressId}>
+                                    <TableCell className="p-4">{location.addressType}</TableCell>
+                                    <TableCell className="p-4">{location.address}</TableCell>
+                                    <TableCell className="p-4">{location.city}</TableCell>
+                                    <TableCell className="p-4">{location.state}</TableCell>
+                                    <TableCell className="p-4">{location.country}</TableCell>
+                                    <TableCell className="p-4">02562</TableCell>
+                                    <TableCell className="p-4">
+                                        <div className="flex space-x-2">
+                                            <Button
+                                                variant="ghost"
+                                                size="icon"
+                                                className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-full h-8 w-8"
+                                                onClick={() => {
+                                                    reset();
+                                                    setEditMode({ isEditing: true, editLocation: location });
+                                                }}
+                                            >
+                                                <i className='fas fa-pencil-alt' />
+                                            </Button>
+                                            <Button
+                                                variant="ghost"
+                                                size="icon"
+                                                className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-full h-8 w-8"
+                                            >
+                                                <i className='fas fa-trash-alt' />
+                                            </Button>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+            )}
+
+            {editMode.isEditing && (
+                <LocationFormInputs
+                    control={control}
+                    errors={errors}
+                    watch={watch}
+                    isLoading={isSubmitting}
+                    setValue={setValue}
+                    setError={setError}
+                    register={register}
+                    onSubmit={handleEditLocation}
+                    getValues={getValues}
+                    values={editMode.editLocation!}
+                />
+            )}
         </div>
     );
 };
